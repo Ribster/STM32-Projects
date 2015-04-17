b6f12f35e4ef3c79129f5312bb7ebc4f2ead9ac7
Ribster <robbevanassche@gmail.com>
2015-04-17 02:35:16 +0200

    GPIO pindefines update

--- An Analog Datalogger REV A/include/projectconfig.h
+++ An Analog Datalogger REV A/include/projectconfig.h
@@ -158,6 +158,102 @@
 98	PE1				Output	GPIO_Output	TESTPOINT 5
 */
 
+#define TESTPOINT_1_PORT GPIOA
+#define TESTPOINT_1_PIN 3
+#define TESTPOINT_1_MODE
+#define TESTPOINT_1_PULL
+#define TESTPOINT_1_OTYPE
+#define TESTPOINT_1_SPEED
+#define TESTPOINT_1_INITSTATE
+
+#define TESTPOINT_2_PORT GPIOA
+#define TESTPOINT_2_PIN 15
+#define TESTPOINT_2_MODE
+#define TESTPOINT_2_PULL
+#define TESTPOINT_2_OTYPE
+#define TESTPOINT_2_SPEED
+#define TESTPOINT_2_INITSTATE
+
+#define TESTPOINT_3_PORT GPIOC
+#define TESTPOINT_3_PIN 13
+#define TESTPOINT_3_MODE
+#define TESTPOINT_3_PULL
+#define TESTPOINT_3_OTYPE
+#define TESTPOINT_3_SPEED
+#define TESTPOINT_3_INITSTATE
+
+#define TESTPOINT_4_PORT GPIOE
+#define TESTPOINT_4_PIN 0
+#define TESTPOINT_4_MODE
+#define TESTPOINT_4_PULL
+#define TESTPOINT_4_OTYPE
+#define TESTPOINT_4_SPEED
+#define TESTPOINT_4_INITSTATE
+
+#define TESTPOINT_5_PORT GPIOE
+#define TESTPOINT_5_PIN 1
+#define TESTPOINT_5_MODE
+#define TESTPOINT_5_PULL
+#define TESTPOINT_5_OTYPE
+#define TESTPOINT_5_SPEED
+#define TESTPOINT_5_INITSTATE
+
+#define TESTPOINT_6_PORT GPIOB
+#define TESTPOINT_6_PIN 4
+#define TESTPOINT_6_MODE
+#define TESTPOINT_6_PULL
+#define TESTPOINT_6_OTYPE
+#define TESTPOINT_6_SPEED
+#define TESTPOINT_6_INITSTATE
+
+#define TESTPOINT_7_PORT GPOIB
+#define TESTPOINT_7_PIN 9
+#define TESTPOINT_7_MODE
+#define TESTPOINT_7_PULL
+#define TESTPOINT_7_OTYPE
+#define TESTPOINT_7_SPEED
+#define TESTPOINT_7_INITSTATE
+
+#define TESTPOINT_8_PORT GPIOD
+#define TESTPOINT_8_PIN 0
+#define TESTPOINT_8_MODE
+#define TESTPOINT_8_PULL
+#define TESTPOINT_8_OTYPE
+#define TESTPOINT_8_SPEED
+#define TESTPOINT_8_INITSTATE
+
+#define TESTPOINT_9_PORT GPIOD
+#define TESTPOINT_9_PIN 1
+#define TESTPOINT_9_MODE
+#define TESTPOINT_9_PULL
+#define TESTPOINT_9_OTYPE
+#define TESTPOINT_9_SPEED
+#define TESTPOINT_9_INITSTATE
+
+#define TESTPOINT_10_PORT GPIOD
+#define TESTPOINT_10_PIN 7
+#define TESTPOINT_10_MODE
+#define TESTPOINT_10_PULL
+#define TESTPOINT_10_OTYPE
+#define TESTPOINT_10_SPEED
+#define TESTPOINT_10_INITSTATE
+
+#define TESTPOINT_11_PORT GPIOD
+#define TESTPOINT_11_PIN 3
+#define TESTPOINT_11_MODE
+#define TESTPOINT_11_PULL
+#define TESTPOINT_11_OTYPE
+#define TESTPOINT_11_SPEED
+#define TESTPOINT_11_INITSTATE
+
+#define TESTPOINT_12_PORT GPIOD
+#define TESTPOINT_12_PIN 4
+#define TESTPOINT_12_MODE
+#define TESTPOINT_12_PULL
+#define TESTPOINT_12_OTYPE
+#define TESTPOINT_12_SPEED
+#define TESTPOINT_12_INITSTATE
+
 // ENCODERS
 /*
 63	PC6			I/O	TIM8_CH1	ENC1_A
@@ -165,29 +261,29 @@
 23	PA0-WKUP	I/O	TIM2_CH1	ENC2_A
 24	PA1			I/O	TIM2_CH2	ENC2_B
 	*/
-#define ENC_1_A_PORT
-#define ENC_1_A_PIN
+#define ENC_1_A_PORT GPIOC
+#define ENC_1_A_PIN 6
 #define ENC_1_A_MODE
 #define ENC_1_A_PULL
 #define ENC_1_A_OTYPE
 #define ENC_1_A_SPEED
 
-#define ENC_1_B_PORT
-#define ENC_1_B_PIN
+#define ENC_1_B_PORT GPIOC
+#define ENC_1_B_PIN 7
 #define ENC_1_B_MODE
 #define ENC_1_B_PULL
 #define ENC_1_B_OTYPE
 #define ENC_1_B_SPEED
 
-#define ENC_2_A_PORT
-#define ENC_2_A_PIN
+#define ENC_2_A_PORT GPIOA
+#define ENC_2_A_PIN 0
 #define ENC_2_A_MODE
 #define ENC_2_A_PULL
 #define ENC_2_A_OTYPE
 #define ENC_2_A_SPEED
 
-#define ENC_2_B_PORT
-#define ENC_2_B_PIN
+#define ENC_2_B_PORT GPIOA
+#define ENC_2_B_PIN 1
 #define ENC_2_B_MODE
 #define ENC_2_B_PULL
 #define ENC_2_B_OTYPE
@@ -197,6 +293,12 @@
 /*
 25	PA2	I/O	ADC1_IN2	BATVOLTAGE
 */
+#define BATVOLTAGE_PORT GPIOA
+#define BATVOLTAGE_PIN 2
+#define BATVOLTAGE_MODE
+#define BATVOLTAGE_PULL
+#define BATVOLTAGE_OTYPE
+#define BATVOLTAGE_SPEED
 
 // AFE
 	/*
@@ -210,64 +312,64 @@
 36	PB1	I/O		GPIO_EXTI1	AFE_SYNC3
 38	PE7	I/O		GPIO_EXTI7	EXT_SYNC
 	 */
-#define AFE_CS_PORT
-#define AFE_CS_PIN
+#define AFE_CS_PORT GPIOA
+#define AFE_CS_PIN 4
 #define AFE_CS_MODE
 #define AFE_CS_PULL
 #define AFE_CS_OTYPE
 #define AFE_CS_SPEED
 
-#define AFE_INT_PORT
-#define AFE_INT_PIN
+#define AFE_INT_PORT GPIOC
+#define AFE_INT_PIN 4
 #define AFE_INT_MODE
 #define AFE_INT_PULL
 #define AFE_INT_OTYPE
 #define AFE_INT_SPEED
 
-#define AFE_MISO_PORT
-#define AFE_MISO_PIN
+#define AFE_MISO_PORT GPIOA
+#define AFE_MISO_PIN 6
 #define AFE_MISO_MODE
 #define AFE_MISO_PULL
 #define AFE_MISO_OTYPE
 #define AFE_MISO_SPEED
 
-#define AFE_MOSI_PORT
-#define AFE_MOSI_PIN
+#define AFE_MOSI_PORT GPIOA
+#define AFE_MOSI_PIN 7
 #define AFE_MOSI_MODE
 #define AFE_MOSI_PULL
 #define AFE_MOSI_OTYPE
 #define AFE_MOSI_SPEED
 
-#define AFE_SCK_PORT
-#define AFE_SCK_PIN
+#define AFE_SCK_PORT GPIOA
+#define AFE_SCK_PIN 5
 #define AFE_SCK_MODE
 #define AFE_SCK_PULL
 #define AFE_SCK_OTYPE
 #define AFE_SCK_SPEED
 
-#define AFE_SYNC1_PORT
-#define AFE_SYNC1_PIN
+#define AFE_SYNC1_PORT GPIOC
+#define AFE_SYNC1_PIN 5
 #define AFE_SYNC1_MODE
 #define AFE_SYNC1_PULL
 #define AFE_SYNC1_OTYPE
 #define AFE_SYNC1_SPEED
 
-#define AFE_SYNC2_PORT
-#define AFE_SYNC2_PIN
+#define AFE_SYNC2_PORT GPIOB
+#define AFE_SYNC2_PIN 0
 #define AFE_SYNC2_MODE
 #define AFE_SYNC2_PULL
 #define AFE_SYNC2_OTYPE
 #define AFE_SYNC2_SPEED
 
-#define AFE_SYNC3_PORT
-#define AFE_SYNC3_PIN
+#define AFE_SYNC3_PORT GPIOB
+#define AFE_SYNC3_PIN 1
 #define AFE_SYNC3_MODE
 #define AFE_SYNC3_PULL
 #define AFE_SYNC3_OTYPE
 #define AFE_SYNC3_SPEED
 
-#define AFE_EXT_SYNC_PORT
-#define AFE_EXT_SYNC_PIN
+#define AFE_EXT_SYNC_PORT GPIOE
+#define AFE_EXT_SYNC_PIN 7
 #define AFE_EXT_SYNC_MODE
 #define AFE_EXT_SYNC_PULL
 #define AFE_EXT_SYNC_OTYPE
@@ -283,43 +385,43 @@
 51	PB12	I/O	SPI2_NSS	nRF_SS
 	*/
 
-#define nRF_CE_PORT
-#define nRF_CE_PIN
+#define nRF_CE_PORT GPIOD
+#define nRF_CE_PIN 9
 #define nRF_CE_MODE
 #define nRF_CE_PULL
 #define nRF_CE_OTYPE
 #define nRF_CE_SPEED
 
-#define nRF_IRQ_PORT
-#define nRF_IRQ_PIN
+#define nRF_IRQ_PORT GPIOD
+#define nRF_IRQ_PIN 8
 #define nRF_IRQ_MODE
 #define nRF_IRQ_PULL
 #define nRF_IRQ_OTYPE
 #define nRF_IRQ_SPEED
 
-#define nRF_MISO_PORT
-#define nRF_MISO_PIN
+#define nRF_MISO_PORT PORTB
+#define nRF_MISO_PIN 14
 #define nRF_MISO_MODE
 #define nRF_MISO_PULL
 #define nRF_MISO_OTYPE
 #define nRF_MISO_SPEED
 
-#define nRF_MOSI_PORT
-#define nRF_MOSI_PIN
+#define nRF_MOSI_PORT PORTB
+#define nRF_MOSI_PIN 15
 #define nRF_MOSI_MODE
 #define nRF_MOSI_PULL
 #define nRF_MOSI_OTYPE
 #define nRF_MOSI_SPEED
 
-#define nRF_SCK_PORT
-#define nRF_SCK_PIN
+#define nRF_SCK_PORT PORTB
+#define nRF_SCK_PIN 13
 #define nRF_SCK_MODE
 #define nRF_SCK_PULL
 #define nRF_SCK_OTYPE
 #define nRF_SCK_SPEED
 
-#define nRF_SS_PORT
-#define nRF_SS_PIN
+#define nRF_SS_PORT PORTB
+#define nRF_SS_PIN 12
 #define nRF_SS_MODE
 #define nRF_SS_PULL
 #define nRF_SS_OTYPE
@@ -333,37 +435,38 @@
 89	PB3	I/O		SPI3_SCK	OLED_SCK
 95	PB8	Output	GPIO_Output	OLED_SS
 	*/
+#define OLED_SPI SPI3
 
-#define OLED_DC_PORT
-#define OLED_DC_PIN
+#define OLED_DC_PORT GPIOB
+#define OLED_DC_PIN 6
 #define OLED_DC_MODE
 #define OLED_DC_PULL
 #define OLED_DC_OTYPE
 #define OLED_DC_SPEED
 
-#define OLED_MOSI_PORT
-#define OLED_MOSI_PIN
+#define OLED_MOSI_PORT GPIOB
+#define OLED_MOSI_PIN 5
 #define OLED_MOSI_MODE
 #define OLED_MOSI_PULL
 #define OLED_MOSI_OTYPE
 #define OLED_MOSI_SPEED
 
-#define OLED_RST_PORT
-#define OLED_RST_PIN
+#define OLED_RST_PORT GPIOB
+#define OLED_RST_PIN 7
 #define OLED_RST_MODE
 #define OLED_RST_PULL
 #define OLED_RST_OTYPE
 #define OLED_RST_SPEED
 
-#define OLED_SCK_PORT
-#define OLED_SCK_PIN
+#define OLED_SCK_PORT GPIOB
+#define OLED_SCK_PIN 3
 #define OLED_SCK_MODE
 #define OLED_SCK_PULL
 #define OLED_SCK_OTYPE
 #define OLED_SCK_SPEED
 
-#define OLED_SS_PORT
-#define OLED_SS_PIN
+#define OLED_SS_PORT GPIOB
+#define OLED_SS_PIN 8
 #define OLED_SS_MODE
 #define OLED_SS_PULL
 #define OLED_SS_OTYPE
@@ -375,22 +478,22 @@
 71	PA12	I/O	USB_OTG_FS_DP
 69	PA10	I/O	USB_OTG_FS_ID
 */
-#define USB_DM_PORT
-#define USB_DM_PIN
+#define USB_DM_PORT GPIOA
+#define USB_DM_PIN 11
 #define USB_DM_MODE
 #define USB_DM_PULL
 #define USB_DM_OTYPE
 #define USB_DM_SPEED
 
-#define USB_DP_PORT
-#define USB_DP_PIN
+#define USB_DP_PORT GPIOA
+#define USB_DP_PIN 12
 #define USB_DP_MODE
 #define USB_DP_PULL
 #define USB_DP_OTYPE
 #define USB_DP_SPEED
 
-#define USB_ID_PORT
-#define USB_ID_PIN
+#define USB_ID_PORT GPIOA
+#define USB_ID_PIN 10
 #define USB_ID_MODE
 #define USB_ID_PULL
 #define USB_ID_OTYPE
@@ -405,6 +508,47 @@
 78	PC10	I/O	SDIO_D2
 79	PC11	I/O	SDIO_D3
 */
+#define SDIO_CK_PORT GPIOC
+#define SDIO_CK_PIN 12
+#define SDIO_CK_MODE
+#define SDIO_CK_PULL
+#define SDIO_CK_OTYPE
+#define SDIO_CK_SPEED
+
+#define SDIO_CMD_PORT GPIOD
+#define SDIO_CMD_PIN 2
+#define SDIO_CMD_MODE
+#define SDIO_CMD_PULL
+#define SDIO_CMD_OTYPE
+#define SDIO_CMD_SPEED
+
+#define SDIO_D0_PORT GPIOC
+#define SDIO_D0_PIN 8
+#define SDIO_D0_MODE
+#define SDIO_D0_PULL
+#define SDIO_D0_OTYPE
+#define SDIO_D0_SPEED
+
+#define SDIO_D1_PORT GPIOC
+#define SDIO_D1_PIN 9
+#define SDIO_D1_MODE
+#define SDIO_D1_PULL
+#define SDIO_D1_OTYPE
+#define SDIO_D1_SPEED
+
+#define SDIO_D2_PORT GPIOC
+#define SDIO_D2_PIN 10
+#define SDIO_D2_MODE
+#define SDIO_D2_PULL
+#define SDIO_D2_OTYPE
+#define SDIO_D2_SPEED
+
+#define SDIO_D3_PORT GPIOC
+#define SDIO_D3_PIN 11
+#define SDIO_D3_MODE
+#define SDIO_D3_PULL
+#define SDIO_D3_OTYPE
+#define SDIO_D3_SPEED
 
 // USART
 /*
@@ -446,6 +590,19 @@
 /*
 8	PC14-OSC32_IN	I/O	RCC_OSC32_IN
 9	PC15-OSC32_OUT	I/O	RCC_OSC32_OUT*/
+#define OSC32_IN_PORT GPIOC
+#define OSC32_IN_PIN 14
+#define OSC32_IN_MODE
+#define OSC32_IN_PULL
+#define OSC32_IN_OTYPE
+#define OSC32_IN_SPEED
+
+#define OSC32_OUT_PORT GPIOC
+#define OSC32_OUT_PIN 15
+#define OSC32_OUT_MODE
+#define OSC32_OUT_PULL
+#define OSC32_OUT_OTYPE
+#define OSC32_OUT_SPEED
 
 // Crystal
 /*
